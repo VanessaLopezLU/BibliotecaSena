341e60974b89cca270893c5f022367367f279974
hassam hernandez <135230835+hesllscream@users.noreply.github.com>
2023-09-12 17:44:12 -0500

    cambios Vanessa 12-09-23

---
+++ app/src/main/java/com/example/bibliotecasena/modelos/Equipo.java
@@ -0,0 +1,10 @@
+package com.example.bibliotecasena.modelos;
+
+public class Equipo {
+     private int id;
+     private  String serial;
+     private String descripcion;
+     private int id_estado;
+     private  int id_tipo;
+
+}
