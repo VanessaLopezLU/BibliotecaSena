180a7c3a3b9d2cead06743f7a5bacd16c37eff1c
hassam hernandez <135230835+hesllscream@users.noreply.github.com>
2023-09-12 16:51:00 -0500

    cambios 12 septiembre 23

---
+++ app/src/main/java/com/example/bibliotecasena/modelos/User.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecasena.modelos;
+
+public class User {
+    private Long cedula;
+    private String nombre;
+    private String apellido;
+    private  String correo;
+    private  Long telefone;
+    private  String usuario;
+    private  String contrasena;
+
+
+}
