936fa9c7e83d982abd662d08ee49bc10d0ff795c
hassam hernandez <135230835+hesllscream@users.noreply.github.com>
2023-09-19 14:23:47 -0500

    cambios Vanessa 19-09-23 actualizacion de las clases

--- app/src/main/java/com/example/bibliotecasena/modelos/User/User.java
+++ app/src/main/java/com/example/bibliotecasena/modelos/User/User.java
@@ -4,10 +4,10 @@ public class User {
     private Long cedula;
     private String nombre;
     private String apellido;
-    private  String correo;
-    private  Long telefone;
-    private  String usuario;
-    private  String contrasena;
-
-
+    private String correo;
+    private Long telefone;
+    private String usuario;
+    private String contrasena;
+    private int[] id_rol;
+    private int[] id_estado;
 }
