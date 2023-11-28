808116fd434914069760bb659c7ffb62a5613945
LuisaArR <luisaarchila123@gmail.com>
2023-11-28 13:39:20 -0500

    actualizacion 28 de noviembre por Ronaldo

--- app/src/main/java/com/example/bibliotecasena/interfaz/PrestamoAPI.java
+++ app/src/main/java/com/example/bibliotecasena/interfaz/PrestamoAPI.java
@@ -1,9 +1,31 @@
 package com.example.bibliotecasena.interfaz;
 
+import com.example.bibliotecasena.modelos.Prestamo.Prestamo;
+import com.example.bibliotecasena.modelos.Rol.Rol;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
 public interface PrestamoAPI {
 
+    @POST("prestamo/crear")
+    <CreatePrestamoDto> // Ruta para crear un prestamo
+    Call<Prestamo> crearPrestamo(@Body CreatePrestamoDto createPrestamoDto);
+
+    @GET("prestamo")
+    Call<List<Prestamo>> ObtenerPrestamo();
+
+    @GET("prestamo/obtenerPrestamoPorCedula ")
+    Call<List<Prestamo>> ObtenerPrestamoPorCeddula();
 
 
-
+    @DELETE("prestamo/{id}") // Ruta para eliminar un prestamo  por ID
+    Call<Void> eliminarPrestamo(@Path("id") int id);
 
 }
